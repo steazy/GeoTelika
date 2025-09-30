2ca99e7b8bac6e99bea2ff7c965a590574a0182c
steazy <steazy@users.noreply.github.com>
2025-09-30 12:17:24 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -44,7 +44,7 @@ app.use(
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",// ✅ was 'production'
+      secure: process.env.NODE_ENV === 'production',// ✅ was 'production'
       sameSite: 'lax',  // ✅ was "strict" - 🔑 allow cookies on redirects and SPA requests
     },
   })
